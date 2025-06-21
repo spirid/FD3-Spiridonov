2af9f8acc2c141adfd2efb324ebb70b001d60a0b
spirid <evan88@mail.ru>
2025-06-21 19:43:15 +0300

    commit shop1 test

---
+++ shop1/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ShopInfo from "./components/info/ShopInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopInfo address="123 Main St" shopName="Anytown" />
+    </div>
+  );
+}
+
+export default App;
