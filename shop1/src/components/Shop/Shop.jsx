44dce0033663d3a6b526e2fc5c8e44f81ab82aea
spirid <evan88@mail.ru>
2025-07-02 19:34:11 +0300

    Кликаем выделяет карточку товара

--- shop1/src/components/Shop/Shop.jsx
+++ shop1/src/components/Shop/Shop.jsx
@@ -3,22 +3,44 @@ import CardItem from "../CardItem/CardItem";
 import "./Shop.css";
 
 class Shop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItemId: null,
+    };
+  }
+
+  handleItemClick = (id) => {
+    if (this.state.selectedItemId !== id) {
+      this.setState({ selectedItemId: id });
+    }
+    // Если убирать выделение нужно даже если клинкуем на тот же элемент раскоментить этот код
+    // this.setState((prevState) => ({ selectedItemId: prevState.selectedItemId === id ? null : id, }));
+  };
+
   render() {
     return (
       <>
         <div className="pizza-list">
           {this.props.products.map((pizza) => (
-            <CardItem
+            <div
               key={pizza.id}
-              id={pizza.id}
-              name={pizza.name}
-              price={pizza.price}
-              imageUrl={
-                pizza.imageUrl || "https://example.com/images/pizza.jpg"
-              }
-              stock={pizza.stock}
-              description={pizza.description}
-            />
+              onClick={() => this.handleItemClick(pizza.id)}
+              className={`card-wrapper ${
+                this.state.selectedItemId === pizza.id ? "selected" : ""
+              }`}
+            >
+              <CardItem
+                id={pizza.id}
+                name={pizza.name}
+                price={pizza.price}
+                imageUrl={
+                  pizza.imageUrl || "https://example.com/images/pizza.jpg"
+                }
+                stock={pizza.stock}
+                description={pizza.description}
+              />
+            </div>
           ))}
         </div>
         <div className="shop-info">
