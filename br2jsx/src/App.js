42dd54301dfe40b507161ad99f1f4c2ed243c91a
spirid <evan88@mail.ru>
2025-07-23 05:50:04 +0300

    3 варинта BR2JSX

--- br2jsx/src/App.js
+++ br2jsx/src/App.js
@@ -3,7 +3,8 @@ import BR2JSX from "./components/BR2JSX/BR2JSX";
 
 class App extends React.Component {
   render() {
-    let text = "первый<br>второй<br/>третий<br />последний";
+    let text =
+      "первый<br>второй<br/>третий<br />последнийпервый<br>второй<br/>третий<br />последнийпервый<br>второй<br/>третий<br />последнийпервый<br>второй<br/>третий<br />последний";
     return <BR2JSX text={text} />;
   }
 }
--- br2jsx/src/components/BR2JSX/BR2JSX.jsx
+++ br2jsx/src/components/BR2JSX/BR2JSX.jsx
@@ -8,21 +8,35 @@ class BR2JSX extends React.Component {
 
     const lines = text.split(/<br\s*\/?>/i);
 
-    const elements = [];
-    for (let i = 0; i < lines.length; i++) {
-      if (lines[i]) {
-        elements.push(lines[i]);
-      }
-      if (i < lines.length - 1) {
-        elements.push(<br key={`br-${i}`} />);
-      }
-    }
+    // 1.
+    // const elements = [];
+    // for (let i = 0; i < lines.length; i++) {
+    //   if (lines[i]) {
+    //     elements.push(lines[i]);
+    //   }
+    //   if (i < lines.length - 1) {
+    //     elements.push(<br key={`br-${i}`} />);
+    //   }
+    // }
 
-    return (
-      <div className="br2jsx" style={{ whiteSpace: "pre-line" }}>
-        {elements}
-      </div>
+    // 2.
+    // const elements = lines
+    //   .map((line, i) => {
+    //     const result = [];
+    //     if (line) result.push(line);
+    //     if (i < lines.length - 1) result.push(<br key={`br-${i}`} />);
+    //     return result;
+    //   })
+    //   .flat();
+
+    const elements = lines.flatMap((line, i) =>
+      [
+        line ? line : null,
+        i < lines.length - 1 ? <br key={`br-${i}`} /> : null,
+      ].filter(Boolean)
     );
+
+    return <div className="br2jsx">{elements}</div>;
   }
 }
 
